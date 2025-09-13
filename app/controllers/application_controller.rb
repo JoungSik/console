1ff93b689868a7ada23dbafb9f5bb5468385fdad
Joungsik <tjstlr2010@gmail.com>
2025-09-14 05:22:10 +0900

    feat: navigation 기능

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   include Authentication
+
+  helper_method :turbo_native_app?
+
+  private
+
+  def turbo_native_app?
+    request.user_agent.to_s.include?("Turbo Native")
+  end
 end
---
+++ app/helpers/navigation_helper.rb
@@ -0,0 +1,62 @@
+module NavigationHelper
+  def navigation_items
+    items = [
+      {
+        name: t("navigation.home"),
+        path: root_path,
+        icon: "home",
+        active_paths: [ root_path ]
+      },
+      {
+        name: t("navigation.collections"),
+        path: collections_path,
+        icon: "folder",
+        active_paths: [ collections_path ]
+      }
+    ]
+
+    # 로그인한 사용자만 볼 수 있는 메뉴
+    if authenticated?
+      items << {
+        name: t("navigation.my_collections"),
+        path: mypage_collections_path,
+        icon: "folder-open",
+        active_paths: [ mypage_collections_path ]
+      }
+      items << {
+        name: t("navigation.profile"),
+        path: "#", # TODO: 프로필 페이지 구현 후 수정
+        icon: "user",
+        active_paths: []
+      }
+    else
+      items << {
+        name: t("navigation.login"),
+        path: new_session_path,
+        icon: "log-in",
+        active_paths: [ new_session_path ]
+      }
+    end
+
+    items
+  end
+
+  def current_navigation_item
+    current_path = request.path
+    navigation_items.find do |item|
+      item[:active_paths].any? { |path| current_path.start_with?(path.split("?").first) }
+    end
+  end
+
+  def navigation_item_active?(item)
+    current_path = request.path
+
+    # 홈페이지는 정확히 일치할 때만 활성화
+    if item[:path] == root_path
+      return current_path == root_path
+    end
+
+    # 다른 페이지들은 start_with 로직 사용
+    item[:active_paths].any? { |path| current_path.start_with?(path.split("?").first) }
+  end
+end
