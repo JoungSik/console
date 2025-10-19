f02e5cda69281086c2b21f74317da991fd8d913a
Joungsik <tjstlr2010@gmail.com>
2025-10-20 01:43:08 +0900

    feat: todos 완료되지 않은 항목이 상위에 오도록 수정

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -1,6 +1,6 @@
 class TodoList < ApplicationRecord
   belongs_to :user
-  has_many :todos, dependent: :destroy
+  has_many :todos, -> { order(completed: :asc, created_at: :asc) }, dependent: :destroy
   accepts_nested_attributes_for :todos, allow_destroy: true, reject_if: :all_blank
 
   has_many :not_completed_todos, -> { not_completed }, class_name: "Todo"
